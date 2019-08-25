a66fa21ca3361ccbf71ada30240b696bfff207b3
Sudhanshu Gautam <me@sudhanshug.com>
2019-08-25 20:06:35 +0530

    Add links for adding issues or contributions (#14)
    Adds a section to the bottom of the page which houses links to ASU's
    issues page and the github repository for the firmware wizard.
    
    Signed-off-by: Sudhanshu Gautam <me@sudhanshug.com>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from './components/header.js';
 import Home from './containers/home/home';
 import NotFound from './containers/not-found/not-found';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { Paper } from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
@@ -34,6 +35,14 @@ function App() {
               <Route default component={NotFound}/>
             </Switch>
           </Router>
+          <Paper elevation={4} className="report-problem-container">
+            <span>
+              If you come across any issue, feel free to report <a href="https://github.com/aparcar/attendedsysupgrade-server/issues">here</a>.
+            </span>
+            <span className="report-link">
+              For contributions, go to <a href="https://github.com/sudhanshu16/openwrt-firmware-selector/">Github</a>
+            </span>
+          </Paper>
         </div>
       </Suspense>
     </ThemeProvider>
